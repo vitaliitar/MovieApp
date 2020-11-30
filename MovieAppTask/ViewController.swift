2227c1f5474e48b776d51243a2f3745248f298f8
vitaliitar <vitalii.tar@uzhnu.edu.ua>
2020-11-30 10:07:56 -0800

    Initial Commit

---
+++ MovieAppTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieAppTask
+//
+//  Created by Vitalii Tar on 11/30/20.
+//  Copyright © 2020 Vitalii Tar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
